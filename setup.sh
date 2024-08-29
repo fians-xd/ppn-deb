29797abf879ec0af16a96acb2c016f39057da76d
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-08-29 09:36:52 +0700

    s

--- setup.sh
+++ setup.sh
@@ -93,6 +93,7 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
 
+echo ""
 echo -e "[ ${BGreen}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
