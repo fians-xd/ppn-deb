5b829077fef9912cf69e59446883209172ca9781
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-09-07 17:24:27 +0700

    ff

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -3,18 +3,18 @@ clear
 cd
 
 #Install Script Websocket-SSH Python
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-dropbear
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-stunnel
+wget --progress=bar:force -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-dropbear 2>&1 | tee /tmp/wget.log | grep --line-buffered -E "HTTP request sent|Length|Saving to|ws-dropbear\s+100%|saved \["
+wget --progress=bar:force -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-stunnel 2>&1 | tee /tmp/wget.log | grep --line-buffered -E "HTTP request sent|Length|Saving to|ws-stunnel\s+100%|saved \["
 
 #izin permision
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
+wget --progress=bar:force -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-dropbear.service 2>&1 | tee /tmp/wget.log | grep --line-buffered -E "HTTP request sent|Length|Saving to|ws-dropbear.service\s+100%|saved \[" && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget --progress=bar:force -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/fians-xd/ppn-deb/master/sshws/ws-stunnel.service 2>&1 | tee /tmp/wget.log | grep --line-buffered -E "HTTP request sent|Length|Saving to|ws-stunnel.service\s+100%|saved \[" && chmod +x /etc/systemd/system/ws-stunnel.service
 
 
 #restart service
