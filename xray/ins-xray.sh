1c6f3ea374571b8758d736ee40b602e169717dcd
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-09-19 12:15:00 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -30,7 +30,7 @@ domain=$(cat /root/domain)
 sleep 0.5
 mkdir -p /etc/xray 
 echo -e "[ ${green}INFO${NC} ] Checking... "
-apt-get install iptables iptables-persistent -y
+apt-get install chrony iptables iptables-persistent -y
 sleep 0.5
 echo " "
 echo -e "[ ${green}INFO$NC ] Setting ntpdate"
