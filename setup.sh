48eb2e5afdc99625693b1b027b42495ae16d5069
fians-xd <152940324+fians-xd@users.noreply.github.com>
2024-09-21 13:21:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -264,7 +264,6 @@ rm -rf setup.sh
 
 cd
 sudo rm -rf /usr/local/bin/shc /tmp/wget.log ins-udp.sh
-sudo apt purge shc -y
 sudo rm -rf /tmp/shc*
 sudo rm -rf /var/tmp/shc*
 #systemctl enable rest_nginx.service
