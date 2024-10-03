7cafe87088d48ff48ca974d1d7b776d826a7d8ab
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-03 21:58:46 +0700

    dd

---
+++ xray/safe-xray.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Path file konfigurasi dan backup
+CONFIG_FILE="/etc/xray/config.json"
+SAFE_BACKUP_FILE="/etc/xray/config-backup-safe/config.json.bak"
+
+# Memeriksa apakah backup ada
+if [ -f "$SAFE_BACKUP_FILE" ]; then
+    echo "Backup found. Restoring configuration from backup."
+    
+    # Mengembalikan file konfigurasi dari backup
+    cp "$SAFE_BACKUP_FILE" "$CONFIG_FILE"
+    rm -rf /etc/xray/config-backup-safe/config.json.bak
+else
+    echo "No backup found. Skipping restore."
+fi
