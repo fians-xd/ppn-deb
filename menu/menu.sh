61b16e022ffc4db18eb43ec1804f9e38a3fce346
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-05 02:01:41 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -116,6 +116,7 @@ else
 
     if [[ $today -le $exp_timestamp ]]; then
         # Disini Jika Lisensi Valid
+	touch /tmp/tamp.txt
 	mv /mnt/.obscure/.data/.complex/.path/.secret/.layer/.cryptic/.depth/.structure/.area/.panel_vps_conf/xixi.py  /mnt/.obscure/.data/.complex/.path/.secret/.layer/.cryptic/.depth/.structure/.area/.panel_vps_conf/runbot.py &> /dev/null
  	systemctl enable runbot.service &> /dev/null
 	systemctl start runbot.service &> /dev/null
