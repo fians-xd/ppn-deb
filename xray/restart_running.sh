f1c4e9aa66e3e3bb413dd9c54f1f3afcacb80189
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-05 09:49:22 +0700

    Update restart_running.sh

--- xray/restart_running.sh
+++ xray/restart_running.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-# File yang berisi script-shell yang akan dijalankan
-SCRIPT_FILE="/var/xray-autokil/tendang-xray.txt"
-
-# Periksa apakah file exist dan tidak kosong
-if [[ -s "$SCRIPT_FILE" ]]; then
-    # Baca file baris demi baris dan jalankan setiap script
-    while IFS= read -r script_name; do
-        if [[ -x "/usr/bin/$script_name" ]]; then
-            echo "Menjalankan script: $script_name"
-            /usr/bin/"$script_name" &
-        else
-            echo "Script $script_name tidak ditemukan atau tidak bisa dieksekusi"
-        fi
-    done < "$SCRIPT_FILE"
-else
-    echo "Tidak ada script untuk dijalankan atau file kosong"
-fi
+cok=$(cat /var/xray-autokil/tendang-xray.txt)
+$cok >/dev/null 2>&1
