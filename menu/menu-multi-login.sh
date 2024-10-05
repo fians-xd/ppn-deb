7a0650a35e73ee53e147b17b7a43f7b4a4e6b647
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-05 07:16:24 +0700

    Update menu-multi-login.sh

--- menu/menu-multi-login.sh
+++ menu/menu-multi-login.sh
@@ -113,6 +113,22 @@ esac
 
 # Tampilkan status setelah menjalankan skrip baru
 sleep 0.9
+
+# File untuk menyimpan status script yang sedang berjalan
+touch /var/xray-autokil/tendang-xray.txt
+STATUS_FILE="/var/xray-autokil/tendang-xray.txt"
+
+# Kosongkan file sebelum mencatat ulang
+> "$STATUS_FILE"
+
+# Cek apakah setiap script sedang berjalan dan simpan ke status file
+for script in menu1-multi-login menu2-multi-login menu3-multi-login menu4-multi-login; do
+    # Periksa apakah script tersebut sedang berjalan menggunakan pidof
+    if pidof "$script" > /dev/null; then
+        echo "$script" >> "$STATUS_FILE"
+    fi
+done
+
 clear
 echo " "
 check_status
