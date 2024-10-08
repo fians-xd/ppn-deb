463435d952940e2a75ff824ac3674e5bbb1b3df4
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-08 13:49:11 +0700

    Update restart_running.sh

--- xray/autokil/restart_running.sh
+++ xray/autokil/restart_running.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cok=$(cat /var/xray-autokil/tendang-xray.txt)
-$cok >/dev/null 2>&1
+# Cek apakah file /var/xray-autokil/tendang-xray.txt ada
+if [ -f /var/xray-autokil/tendang-xray.txt ]; then
+    # Jika file ada, baca isi file dan eksekusi perintah di dalamnya
+    cok=$(cat /var/xray-autokil/tendang-xray.txt)
+    $cok >/dev/null 2>&1
+fi
