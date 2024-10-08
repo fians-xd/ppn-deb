ccf92307014aa16b1b6930d2d7f26bfd64013505
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-08 17:31:46 +0700

    Update restore-xray-config.sh

--- xray/autokil/restore-xray-config.sh
+++ xray/autokil/restore-xray-config.sh
@@ -3,15 +3,24 @@
 # Definisi variabel
 CONFIG_FILE="/etc/xray/config.json"
 
-# Fungsi untuk menghapus tanda "-" di awal dan akhir nama pengguna yang terdeteksi
-remove_dashes_from_usernames() {
-    echo "Memulihkan nama pengguna yang memiliki tanda '-' di awal dan akhir."
-    
-    # Hapus tanda "-" di awal dan akhir nama pengguna di file konfigurasi
-    sed -i 's/\"email\": \"-\(.*\)-\"/\"email\": \"\1\"/' $CONFIG_FILE
-    
+# Fungsi untuk menghapus tanda "✓" dari ID dan password pengguna
+remove_marks_from_users() {
+    echo "Memulihkan ID dan password pengguna dengan menghapus tanda '✓'."
+
+    # Hapus tanda "✓" dari password Trojan
+    if grep -q "\"password\": " "$CONFIG_FILE"; then
+        sed -i -E 's/(\"password\": \"✓)([^\"]*)(✓\")/\1\2\3/' $CONFIG_FILE
+    fi
+
+    # Hapus tanda "✓" dari ID VLess dan VMess
+    for prefix in "#&" "###"; do
+        if grep -q "^$prefix " "$CONFIG_FILE"; then
+            sed -i -E 's/(\"email\": \"✓)(.*?)(✓\")/\1\2\3/' $CONFIG_FILE
+        fi
+    done
+
     systemctl restart xray
 }
 
-# Panggil fungsi untuk menghapus tanda "-" jika ditemukan
-remove_dashes_from_usernames
+# Panggil fungsi untuk menghapus tanda "✓" jika ditemukan
+remove_marks_from_users
