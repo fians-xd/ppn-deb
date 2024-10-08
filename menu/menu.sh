beb9e12bd18207302af92bc968f42541204ab09f
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-08 21:02:13 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -219,13 +219,13 @@ echo -e "\e[1;32m  SSH\e[31m:\e[0m$xshx    \e[1;32mVmess\e[31m:\e[0m$xvmesx    \
 echo -e "\e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo -e "\e[1;44m                   ━MENU━                      \e[0m"
 echo -e "\e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
-echo -e "\e[1;33m [\e[36m1\e[1;33m]\e[31m : \e[0mSSH                \e[1;33m[\e[36m8 \e[1;33m]\e[31m : \e[0mReboot VPS"
-echo -e "\e[1;33m [\e[36m2\e[1;33m]\e[31m : \e[0mVmess              \e[1;33m[\e[36m9 \e[1;33m]\e[31m : \e[0mMonitor VPS"
-echo -e "\e[1;33m [\e[36m3\e[1;33m]\e[31m : \e[0mVless              \e[1;33m[\e[36m10 \e[1;33m]\e[31m : \e[0mStatus Service"
-echo -e "\e[1;33m [\e[36m4\e[1;33m]\e[31m : \e[0mTrojan             \e[1;33m[\e[36m11 \e[1;33m]\e[31m: \e[0mClear Cache"
-echo -e "\e[1;33m [\e[36m5\e[1;33m]\e[31m : \e[0mShwsocks           \e[1;33m[\e[36m12 \e[1;33m]\e[31m: \e[0mBoot Telegram"
-echo -e "\e[1;33m [\e[36m6 \e[1;33m]\e[31m : \e[0mAutokil Ssh        \e[1;33m[\e[36m13 \e[1;33m]\e[31m : \e[0mSetting Script"
-echo -e "\e[1;33m [\e[36m7 \e[1;33m]\e[31m : \e[0mAutokil Xray       \e[1;33m[\e[36mX \e[1;33m]\e[31m : \e[0mExit Script"
+echo -e "\e[1;33m [\e[36m1\e[1;33m]\e[31m : \e[0mSSH                \e[1;33m[\e[36m8\e[1;33m]\e[31m : \e[0mReboot VPS"
+echo -e "\e[1;33m [\e[36m2\e[1;33m]\e[31m : \e[0mVmess              \e[1;33m[\e[36m9\e[1;33m]\e[31m : \e[0mMonitor VPS"
+echo -e "\e[1;33m [\e[36m3\e[1;33m]\e[31m : \e[0mVless              \e[1;33m[\e[36m10\e[1;33m]\e[31m : \e[0mStatus Service"
+echo -e "\e[1;33m [\e[36m4\e[1;33m]\e[31m : \e[0mTrojan             \e[1;33m[\e[36m11\e[1;33m]\e[31m : \e[0mClear Cache"
+echo -e "\e[1;33m [\e[36m5\e[1;33m]\e[31m : \e[0mShwsocks           \e[1;33m[\e[36m12\e[1;33m]\e[31m : \e[0mBoot Telegram"
+echo -e "\e[1;33m [\e[36m6\e[1;33m]\e[31m : \e[0mAutokil Ssh        \e[1;33m[\e[36m13\e[1;33m]\e[31m : \e[0mSetting Script"
+echo -e "\e[1;33m [\e[36m7\e[1;33m]\e[31m : \e[0mAutokil Xray       \e[1;33m[\e[36mX\e[1;33m]\e[31m : \e[0mExit Script"
 echo -e "\e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo -e "\e[1;44m                 ━USER INFO━                   \e[0m"
 echo -e "\e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
@@ -251,6 +251,6 @@ case $opt in
 11) clear ; clearcache ;;
 12) clear ; tambah_bot ;;
 13) clear ; m-system ;;
-X) exit ;;
+X|x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
