4d4d16bb291e334a2d8672902dd749484a52986c
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-08 10:36:25 +0700

    Delete xray/menu1-multi-login.sh

--- xray/menu1-multi-login.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-    ip_limit=$(cat /var/xray-autokil/ip_limit.txt)
-    # Jalankan skrip untuk deteksi multi-login
-    detect-multi-login 5 $ip_limit
-    sleep 5
-done
