6f1426507ad6f1aca844808cb1f7a68db95eb25f
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-15 14:31:49 +0700

    Delete xray/autokil directory

--- xray/autokil/restart_running.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Cek apakah file /var/xray-autokil/tendang-xray.txt ada
-if [ -f /var/xray-autokil/tendang-xray.txt ]; then
-    # Jika file ada, baca isi file dan eksekusi perintah di dalamnya
-    cok=$(cat /var/xray-autokil/tendang-xray.txt)
-    $cok > /dev/null 2>&1
-fi
