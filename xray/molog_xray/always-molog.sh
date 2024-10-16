26c6498fbbb12b88b87a9241374fb4b6fb5506a9
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-16 23:16:00 +0700

    Update always-molog.sh

--- xray/molog_xray/always-molog.sh
+++ xray/molog_xray/always-molog.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# Membaca isi file
+durasi=$(cat ~/.molog-xray/durasi.txt)
+limit=$(cat ~/.molog-xray/limit.txt)
+
+# Mengecek apakah file durasi.txt dan limit.txt tidak kosong
+if [[ -n "$durasi" && -n "$limit" ]]; then
+    echo "File tidak kosong. Menjalankan perintah lock-molog..."
+    # Ganti dengan perintah yang ingin dijalankan
+    lock-molog
+else
+    echo "Salah satu atau kedua file kosong. Tidak menjalankan perintah."
+fi
