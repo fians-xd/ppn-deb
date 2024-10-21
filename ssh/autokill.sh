421ef5cef215786ab84c8f2e7218dda3fec59643
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-21 22:41:43 +0700

    Update autokill.sh

--- ssh/autokill.sh
+++ ssh/autokill.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-echo "Checking VPS"
+
 clear
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
