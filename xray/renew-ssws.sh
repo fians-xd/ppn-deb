755375981ac1a60ea8e2b51f884411bcb8588dab
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-23 00:07:45 +0700

    Update renew-ssws.sh

--- xray/renew-ssws.sh
+++ xray/renew-ssws.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# SL
-# ==========================================
+
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
@@ -10,18 +9,7 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl ipv4.icanhazip.com | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
+
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
