dd7571b5f40c9cbdf2d30872cb66c140b4383fb8
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-23 00:02:55 +0700

    Update del-ssws.sh

--- xray/del-ssws.sh
+++ xray/del-ssws.sh
@@ -41,12 +41,12 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo -e "\E[44;1;39m       Delete Sodosok Account      \E[0m"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo "  User       Expired  " 
-	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-    echo ""
-	echo -e "  • [NOTE] Press any key to back on menu"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	read -rp "Input Username : " user
+    grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e " [NOTE] Press any key to back on menu"
+    echo " "
+    read -rp " Input Username: " user
     if [ -z $user ]; then
     m-ssws
     else
