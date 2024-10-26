bd0401cea7e1b43228a0cb9dadc14c75190c1dfa
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-26 13:20:49 +0700

    Update ingpo.sh

--- menu/ingpo.sh
+++ menu/ingpo.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # Mengambil informasi sistem
+mapfile -t API_KEYS < /usr/bin/geolocation.txt
+API_KEY=${API_KEYS[$RANDOM % ${#API_KEYS[@]}]}
+response=$(curl -s "https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}")
 OS=$(lsb_release -sd | sed 's/ GNU\/Linux//g' | awk '{print $1, $2, $3}')
 UPTIME=$(uptime -p | sed 's/^up //')
 PUBLIC_IP=$(curl -s ifconfig.me)
-citytx=$(curl -s ipinfo.io/city)
-count_codex=$(curl -s ipinfo.io/country)
-country_namez=$(grep -w $count_codex /usr/bin/countriest.txt | awk '{print $2}')
 DOMAIN=$(cat /etc/xray/domain)
 DATE_TIME=$(date '+%d %b %Y %H:%M')
-ISP=$(curl -s ipinfo.io | jq -r '.org' | awk -F' ' '{$1=""; print substr($0,2)}')
+ISP=$(echo "$response" | jq -r '.organization')
+cityt=$(echo "$response" | jq -r '.city')
+count_name=$(echo "$response" | jq -r '.country_name')
 
 # Menyimpan informasi ke file log
 {
     echo "◆ 𝐎𝐒 : $OS"
     echo "◆ 𝐏𝐮𝐛-𝐈𝐏 : $PUBLIC_IP"
     echo "◆ 𝐃𝐨𝐦𝐚𝐢𝐧 : $DOMAIN"
-    echo "◆ 𝐂𝐨𝐮𝐧𝐭 : $citytx, $country_namez"
+    echo "◆ 𝐂𝐨𝐮𝐧𝐭 : $cityt, $count_name"
     echo "◆ 𝐔𝐩𝐭𝐢𝐦𝐞 : $UPTIME"
     echo "◆ 𝐈𝐒𝐏 : $ISP"
     echo "◆ 𝐃𝐚𝐭𝐞&𝐓𝐢𝐦𝐞 : $DATE_TIME"
