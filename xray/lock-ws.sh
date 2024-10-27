c87660943ba77c518f314a640ed9e9acf79b25d1
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-27 23:21:41 +0700

    Update lock-ws.sh

--- xray/lock-ws.sh
+++ xray/lock-ws.sh
@@ -57,7 +57,7 @@ else
   # Cek apakah akun sudah dikunci (mengandung # di depan)
   if grep -q "#},{\"id\":.*\"email\": \"$user\"" "$CONFIG_FILE"; then
     # Jika akun sudah dikunci, abaikan dan tidak cetak apa-apa
-    exit
+    echo -e " Akun $user Sudah Dikunci Asw"
   else
     # Jika akun tidak dikunci, kunci akun dengan menambahkan tanda komentar
     sed -i "/},{\"id\":.*\"email\": \"$user\"/s/},{/#},{/" "$CONFIG_FILE"
