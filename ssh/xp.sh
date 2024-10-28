f5243e0765ea10bba30b81da0ed6179531bb1533
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-28 12:41:42 +0700

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -2,54 +2,58 @@
 
 clear
 #----- Auto Remove Vmess
-data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-        # Hapus yang memiliki tanda `},{`
-        sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-        # Hapus yang memiliki tanda `#},{`
-        sed -i "/^### $user $exp/,/^#},{/d" /etc/xray/config.json
-        rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
-    fi
-done
+data=( $(grep '^###' /etc/xray/config.json | cut -d ' ' -f 2 | sort | uniq) )
+now=$(date +"%Y-%m-%d")
+if [[ ${#data[@]} -gt 0 ]]; then
+  for user in "${data[@]}"; do
+      exp=$(grep -w "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+      d1=$(date -d "$exp" +%s)
+      d2=$(date -d "$now" +%s)
+      exp2=$(( (d1 - d2) / 86400 ))
+      if [[ "$exp2" -le "0" ]]; then
+          # Hapus yang memiliki tanda `},{`
+          sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+          # Hapus yang memiliki tanda `#},{`
+          sed -i "/^### $user $exp/,/^#},{/d" /etc/xray/config.json
+          rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
+      fi
+  done
+fi
 
 #----- Auto Remove Vless
-data=( `cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-        # Hapus yang memiliki tanda `},{`
-        sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
-        # Hapus yang memiliki tanda `#},{`
-        sed -i "/^#& $user $exp/,/^#},{/d" /etc/xray/config.json
-    fi
-done
+data=( $(grep '^#&' /etc/xray/config.json | cut -d ' ' -f 2 | sort | uniq) )
+now=$(date +"%Y-%m-%d")
+if [[ ${#data[@]} -gt 0 ]]; then
+  for user in "${data[@]}"; do
+      exp=$(grep -w "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+      d1=$(date -d "$exp" +%s)
+      d2=$(date -d "$now" +%s)
+      exp2=$(( (d1 - d2) / 86400 ))
+      if [[ "$exp2" -le "0" ]]; then
+          # Hapus yang memiliki tanda `},{`
+          sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
+          # Hapus yang memiliki tanda `#},{`
+          sed -i "/^#& $user $exp/,/^#},{/d" /etc/xray/config.json
+      fi
+  done
+fi
 
 #----- Auto Remove Trojan
-data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`)
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-        # Hapus akun yang sudah kedaluwarsa dengan tanda `},{` atau `#},{` sebagai pemisah
-        sed -i "/^#! $user $exp$/,/^\s*#\{0,1\}},{/d" /etc/xray/config.json
-    fi
-done
+data=( $(grep '^#!' /etc/xray/config.json | cut -d ' ' -f 2 | sort | uniq) )
+now=$(date +"%Y-%m-%d")
+if [[ ${#data[@]} -gt 0 ]]; then
+  for user in "${data[@]}"; do
+      exp=$(grep -w "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+      d1=$(date -d "$exp" +%s)
+      d2=$(date -d "$now" +%s)
+      exp2=$(( (d1 - d2) / 86400 ))
+      if [[ "$exp2" -le "0" ]]; then
+          # Hapus akun yang sudah kedaluwarsa dengan tanda `},{` atau `#},{` sebagai pemisah
+          sed -i "/^#! $user $exp$/,/^\s*#\{0,1\}},{/d" /etc/xray/config.json
+      fi
+  done
+fi
+
 systemctl restart xray
 
 ##------ Auto Remove SSH
