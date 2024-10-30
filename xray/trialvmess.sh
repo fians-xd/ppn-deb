d587ba7b380795100298a57a004cdd88a75ee451
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-30 17:54:12 +0700

    Update trialvmess.sh

--- xray/trialvmess.sh
+++ xray/trialvmess.sh
@@ -57,12 +57,14 @@ grpc=`cat<<EOF
       "tls": "tls"
 }
 EOF`
+
 vmess_base641=$( base64 -w 0 <<< $vmess_json1)
 vmess_base642=$( base64 -w 0 <<< $vmess_json2)
 vmess_base643=$( base64 -w 0 <<< $vmess_json3)
 vmesslink1="vmess://$(echo $asu | base64 -w 0)"
 vmesslink2="vmess://$(echo $ask | base64 -w 0)"
 vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
+
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 clear
