763b58e34f808dcca10af23cd105683778dd82fc
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-31 19:45:31 +0700

    Update m-vmess.sh

--- menu/m-vmess.sh
+++ menu/m-vmess.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\e[1;44m           ━VMESS MENU━            \e[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m1\e[0m] Create Account Vmess "
 echo -e " [\e[36m2\e[0m] Trial Account Vmess "
@@ -18,7 +18,7 @@ echo -e ""
 echo -e " [\e[31m0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
 echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -p " Pilih menu:  "  opt
 echo -e ""
