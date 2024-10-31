12ed12c17e89916923d80de7725c7b4658cb927a
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-31 20:08:43 +0700

    Update add-host.sh

--- ssh/add-host.sh
+++ ssh/add-host.sh
@@ -3,19 +3,19 @@
 clear
 cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo ""
 read -rp "Input Domain Baru: " -e host
 echo ""
 if [ -z $host ]; then
 echo "????"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 read -n 1 -s -r -p "Press any key to back on menu"
 m-domain
 else
 echo "IP=$host" > /var/lib/ipvps.conf
 echo "$host" > /root/domain
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo "Dont forget to renew cert"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
