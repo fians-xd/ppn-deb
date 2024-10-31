895affc38e685f296f0e00000e76b3466a10a4db
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-10-31 19:48:44 +0700

    Update monitor.sh

--- menu/monitor.sh
+++ menu/monitor.sh
@@ -2,10 +2,10 @@
 
 # Menampilkan pesan sebelum menjalankan htop
 echo ""
-echo -e "   \e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
+echo -e "   \e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo -e "          \e[1;36mTekan Ctrl+C\e[0m"
 echo -e "   \e[1;36muntuk keluar dari monitoring.!\e[0m"
-echo -e "   \e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
+echo -e "   \e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo ""
 read -n 1 -s -r -p "         Silahkan Enter.!"
 
