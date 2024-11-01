a1763002be60117df537c61c31a5f23fecce673a
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-11-01 09:17:51 -0700

    Update listcreat-tr.sh

--- xray/listcreat-tr.sh
+++ xray/listcreat-tr.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [ ! -f /etc/log-create-trojan.log ] || [ ! -s /etc/log-create-trojan.log ]; then
+    echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo " "
     echo " Anda Belum Menambahkan User Trojan.."
+    echo " "
+    echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 else
     cat /etc/log-create-trojan.log
 fi
