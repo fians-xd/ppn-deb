3f86668eeae62393bb068d056d2f8218322f88bf
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-11-01 09:15:51 -0700

    Update listcreat-ssh.sh

--- ssh/listcreat-ssh.sh
+++ ssh/listcreat-ssh.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [ ! -f /etc/log-create-ssh.log ] || [ ! -s /etc/log-create-ssh.log ]; then
+    echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo " "
-    echo "Anda Belum Menambahkan User Ssh-Ws Tls/Ntls.."
+    echo " Anda Belum Menambahkan User Ssh-Ws Tls/Ntls.."
+    echo " "
+    echo -e "\e[1;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 else
     cat /etc/log-create-ssh.log
 fi
