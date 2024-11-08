47902650c24309e68cd6b7afdae6b8819da2462d
Sofian Nasution <152940324+fians-xd@users.noreply.github.com>
2024-11-08 08:46:06 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -436,8 +436,8 @@ cat > /etc/cron.d/rlog_otm <<-END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 */5 * * * * root > /var/log/xray/access.log
-0 23 * * * root > /var/log/auth.log
-0 23 * * * root > /var/log/secure
+*/5 * * * * root > /var/log/auth.log
+*/5 * * * * root > /var/log/secure
 */5 * * * * root > /etc/cek-tr.log
 */5 * * * * root > /etc/cek-vless.log
 */5 * * * * root > /etc/cek-vmess.log
